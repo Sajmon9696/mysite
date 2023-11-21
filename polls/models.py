37a06e1d633179fc621c4c51509413e74d1934e6
Sajmon9696 <Sajmon9696@noreply.github.com>
2023-11-21 19:19:31 +0100

    Added initial project

---
+++ polls/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+# echo "# mysite" >> README.md
+#   git init
+#   git add README.md
+#   git commit -m "first commit"
+#   git branch -M main
+#   git remote add origin https://github.com/Sajmon9696/mysite.git
+#   git push -u origin main
