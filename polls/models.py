aa4bfaae46fb612199fa57d1566c3bb5c44950eb
Sajmon9696 <Sajmon9696@noreply.github.com>
2023-11-21 20:56:45 +0100

    Added first models

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
-# Create your models here.
-# echo "# mysite" >> README.md
-#   git init
-#   git add README.md
-#   git commit -m "first commit"
-#   git branch -M main
-#   git remote add origin https://github.com/Sajmon9696/mysite.git
-#   git push -u origin main
+
+
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
