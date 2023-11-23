6e764c01d8dae330a0ef1c3974124caa07c95f3c
Sajmon9696 <Sajmon9696@noreply.github.com>
2023-11-23 19:52:30 +0100

    Fixed vote fuc mistake

--- polls/views.py
+++ polls/views.py
@@ -21,16 +21,29 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the result of question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choise = question.choice_set.get(pk=request.POST["choice"])
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        render(request, "polls/detail.html", {"question": question, "error_message": "You didn't selected a chose "})
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
     else:
-        selected_choise.choice.votes +=1
-        selected_choise.save()
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
